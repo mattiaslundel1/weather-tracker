134bd3523fe78aa555250035f96101e154c53d47
mattiaslundel1 <mattias.lundell@elva-group.com>
2024-01-11 18:59:58 +0100

    some polishing

--- src/functions/pollSMHI.ts
+++ src/functions/pollSMHI.ts
@@ -1,13 +1,13 @@
 import z from "zod";
 import { cities } from "../data/cities";
 
-const SMHI_ENDPOINT = "https://opendata-download-metfcst.smhi.se/api/category/pmp3g/version/2/geotype/point/lon/11.59/lat/57.43/data.json"
+const SMHI_ENDPOINT = "https://opendata-download-metfcst.smhi.se/api/category/pmp3g/version/2/geotype/point"
 
 export const handler = async (): Promise< {airTemp: number, timeStamp: string } | null> => {
   console.log("Polling weather data from SMHI!")
 
   const response = await (
-    await fetch(`https://opendata-download-metfcst.smhi.se/api/category/pmp3g/version/2/geotype/point/lon/${cities[0].longitude}/lat/${cities[0].latitude}/data.json`)
+    await fetch(`${SMHI_ENDPOINT}/lon/${cities[0].longitude}/lat/${cities[0].latitude}/data.json`)
     ).json();
     
   const result = responseSchema.safeParse(response);
@@ -17,13 +17,10 @@ export const handler = async (): Promise< {airTemp: number, timeStamp: string } 
     return null;
   }
 
-  const {
-    name,
-    values,
-  } = result.data.timeSeries[0].parameters[10];
+  const airTemp = result.data.timeSeries[0].parameters[10].values[0];
   const timeStamp = result.data.timeSeries[0].validTime;
 
-  return {airTemp: values[0], timeStamp: timeStamp}
+  return {airTemp: airTemp, timeStamp: timeStamp}
 
 };
 
--- src/functions/pollYR.ts
+++ src/functions/pollYR.ts
@@ -1,13 +1,13 @@
 import z from "zod";
 import { cities } from "../data/cities";
 
-const YR_ENDPOINT = "https://api.met.no/weatherapi/locationforecast/2.0/compact.json?"
+const YR_ENDPOINT = "https://api.met.no/weatherapi/locationforecast/2.0/compact.json"
 
 export const handler = async (): Promise< {airTemp: number, timeStamp: string} | null > => {
   console.log("Polling weather data from YR.no!")
 
   const response = await (
-    await fetch(`${YR_ENDPOINT}lat=${cities[0].latitude}&lon=${cities[0].longitude}`)
+    await fetch(`${YR_ENDPOINT}?lat=${cities[0].latitude}&lon=${cities[0].longitude}`)
     ).json();
 
     const result = responseSchema.safeParse(response);
@@ -17,14 +17,7 @@ export const handler = async (): Promise< {airTemp: number, timeStamp: string} |
       return null;
     }
 
-    const {
-      air_pressure_at_sea_level,
-      air_temperature,
-      cloud_area_fraction,
-      relative_humidity,
-      wind_from_direction,
-      wind_speed,
-    } = result.data.properties.timeseries[0].data.instant.details;
+    const air_temperature = result.data.properties.timeseries[0].data.instant.details.air_temperature;
 
     const timeStamp = result.data.properties.timeseries[0].time
 
@@ -39,12 +32,7 @@ const responseSchema = z.object({
         data: z.object({
           instant: z.object({
             details: z.object({
-              air_pressure_at_sea_level: z.number(),
               air_temperature: z.number(),
-              cloud_area_fraction: z.number(),
-              relative_humidity: z.number(),
-              wind_from_direction: z.number(),
-              wind_speed: z.number(),
             }),
           }),
         }),
