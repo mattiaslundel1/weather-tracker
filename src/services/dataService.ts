80f678938221c34efd52d3d5536ba71d4c9beaf8
mattiaslundel1 <mattias.lundell@elva-group.com>
2024-01-26 14:56:07 +0100

    feat(dataService): added a new type of assistant

--- src/services/dataService.ts
+++ src/services/dataService.ts
@@ -1,9 +1,9 @@
-import { Config } from "sst/node/config";
-import { OpenAI } from "openai";
+import { Config } from 'sst/node/config';
+import { OpenAI } from 'openai';
 
 const OPEN_AI_KEY = Config.OPEN_AI_KEY;
 /**
- * 
+ *
  * @param {string} city - What city the assistant should make a recommendation for
  * @param {string} timeStamp - What timestamp (UTC-format) the assistan should take into consideration
  * @param {string} airTemperature - The average air temperature the assistant should base its recommendation on
@@ -14,18 +14,20 @@ const generateRecommendation = async (
   timeStamp: string,
   airTemperature: string,
   windSpeed: string,
-  precipitation: string
+  precipitation: string,
 ): Promise<string> => {
   const openAI = new OpenAI({ apiKey: OPEN_AI_KEY });
 
+  const SASSY = `You are a sassy personal stylist. You should recommend how to dress to stay hip, cool and comfortable based on the time and date (${timeStamp}), location ${city}, and weather data( air temperature = ${airTemperature} °C, wind speed = ${windSpeed} m/s, precipitation = ${precipitation} mm/h). Start your message with a greeting to Elva Gothenburg! You are addressing a group of both males and females. Be witty and make jokes, but keep it brief. Use a maximum of 300 characters. You should always include the temperature and add emojis that suits the weather and your recommendation.`;
+
+  const PEAKY = `You are a british foot soldier of a old school gang from the world war one era. Use British slang, Cockney, and swear words. Be foul mouthed You should recommend how to dress proper based on the time and date (${timeStamp}), location ${city}, and weather data( air temperature = ${airTemperature} °C, wind speed = ${windSpeed} m/s, precipitation = ${precipitation} mm/h). Start your message with a greeting to Elva Gothenburg. Use a maximum of 300 characters. Include the temperature when giving your recommendation. End with Cheers and emoji of beer and the union jack.`;
   /**
    * Instructions on what behaviour the OpenAI assistant should have. The AI base model is also specified.
    */
   const weatherStylist = await openAI.beta.assistants.create({
-    name: "Weather Stylist",
-    instructions:
-      `You are a sassy personal stylist, a real alpha Chad. You should recommend how to dress to stay hip, cool and comfortable based on the time and date (${timeStamp}), location ${city}, and weather data( air temperature = ${airTemperature} °C, wind speed = ${windSpeed} m/s, precipitation = ${precipitation} mm/h). Start your message with a greeting to Elva Gothenburg! You are addressing a group of both males and females. Be witty and make jokes, but keep it brief. Use a maximum of 300 characters. You should always include the temperature and add emojis that suits the weather and your recommendation.`,
-    model: "gpt-3.5-turbo",
+    name: 'Weather Stylist',
+    instructions: PEAKY,
+    model: 'gpt-3.5-turbo',
   });
 
   /**
@@ -39,33 +41,34 @@ const generateRecommendation = async (
    * The response takes ~10-15s to generate
    */
   let run = await openAI.beta.threads.runs.retrieve(
-    executeRun.thread_id, executeRun.id
-  )
-  let runStatus = run.status
+    executeRun.thread_id,
+    executeRun.id,
+  );
+  let runStatus = run.status;
 
   /**
    * In order to fetch the response, a while loop is initiated. The loop will run as long as the status is queued or in_progress. For each loop, we wait 1 second before trying again.
    */
-  while(runStatus === "queued" || runStatus === 'in_progress') {
-    console.log("runStatus IN LOOP: ", runStatus)
-    
+  while (runStatus === 'queued' || runStatus === 'in_progress') {
     if (runStatus === 'queued' || runStatus === 'in_progress') {
       // Sleep for a short duration (e.g., 1 second) before checking again
       await new Promise((resolve) => setTimeout(resolve, 1000));
     }
 
     run = await openAI.beta.threads.runs.retrieve(
-      executeRun.thread_id, executeRun.id
-    )
-    runStatus = run.status
+      executeRun.thread_id,
+      executeRun.id,
+    );
+    runStatus = run.status;
   }
 
   /**
    * The response is finally fetched and filtered out from the response. The assistant is then deleted to save on storage at OpenAI, there is no point in keeping it alive as it only work 1 time a day.
    */
-  const messages = await openAI.beta.threads.messages.list(run.thread_id)
-  const answer = messages.data[0].content[0] as OpenAI.Beta.Threads.MessageContentText
-  
+  const messages = await openAI.beta.threads.messages.list(run.thread_id);
+  const answer = messages.data[0]
+    .content[0] as OpenAI.Beta.Threads.MessageContentText;
+
   await openAI.beta.assistants.del(weatherStylist.id);
 
   return answer.text.value;
